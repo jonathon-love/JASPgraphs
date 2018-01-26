83b1308ad7f8542b9c7c830da4fd536b89b35eff
vandenman <donvdbergh@hotmail.com>
2018-01-26 11:25:26 +0100

    some more updates to ggMatrix

--- R/ggMatrixPlot.R
+++ R/ggMatrixPlot.R
@@ -59,7 +59,36 @@ makeRect <- function(col = "red", size = 2, fill = scales::alpha("black", 0)) {
     ))
 }
 
-makeLabel <- function(label, angle = 0, size = 1, family = "serif") {
+replicateOrStop <- function(x, n) {
+    
+    if (length(x) == 1) {
+        x <- rep(x, n)
+    } else if (length(x) != n) {
+        stop(sprintf(
+            fmt = "Argument %s should either have length 1 or length %d.", 
+            deparse(substitute(x)), n
+        ))
+    }
+        
+    return(x)
+}
+
+#' @export
+makeLabels <- function(label, angle = 0, size = 1, family = "serif") {
+    
+    UseMethod("makeLabels", label)
+    
+}
+
+#' @export
+makeLabels.default <- function(label, angle = 0, size = 1, family = "serif") {
+    
+    if (is.null(label))
+        return(NULL)
+    
+    # recursive vectorization
+    if (length(label) > 1)
+        return(lapply(label, makeLabels.default, angle = angle, size = size, family = family))
     
     # draws text in center (!) of plot.
     df <- data.frame(x=0, y=0.0)
@@ -78,81 +107,202 @@ makeLabel <- function(label, angle = 0, size = 1, family = "serif") {
     )
 }
 
+#' @export
+makeLabels.list <- function(label, angle = 0, size = 1, family = "serif") {
+    
+    nLabels <- length(label)
+    angle <- replicateOrStop(angle, nLabels)
+    size <- replicateOrStop(size, nLabels)
+    output <- vector("list", nLabels)
+    names(output) <- names(label)
+
+    for (i in seq_along(label)) {
+        
+        if (is.null(label[[i]]))
+            next
+        
+        output[[i]] <- makeLabels.default(label[[i]], angle = angle[[i]], size = size[[i]], family = family)
+        
+    }
+    
+    return(output)
+    
+}
+
 
 #' @export
-ggMatrixPlot <- function(plotList = NULL, nr, nc,
-    addRightLabels = FALSE,
-    addBottomLabels = FALSE,
-    debug = TRUE) {
+ggMatrixPlot <- function(plotList = NULL, nr = NULL, nc = NULL,
+                         ...,
+                         leftLabels = NULL,
+                         topLabels = NULL,
+                         rightLabels = NULL,
+                         bottomLabels = NULL,
+                         debug = TRUE) {
+    UseMethod("ggMatrixPlot", plotList)
+}
+
+#' @export
+ggMatrixPlot.matrix <- function(plotList = NULL, nr = NULL, nc = NULL,
+                                layout = NULL,
+                                ...,
+                                leftLabels = NULL,
+                                topLabels = NULL,
+                                rightLabels = NULL,
+                                bottomLabels = NULL,
+                                debug = TRUE) {
+
+    # dim cannot be NULL since plotList is a matrix
+    nr <- dim(plotList)[1L]
+    nc <- dim(plotList)[2L]
+    dimNms <- dimnames(plotList)
+
+    if (!is.null(dimNms[[1L]]) && is.null(leftLabels))
+        leftLabels <- dimNms[[1L]]
     
-    # todo: add "addLeftLabels" and "addTopLabels"
+    if (!is.null(dimNms[[2L]]) && is.null(topLabels))
+        topLabels <- dimNms[[2L]]
+
+    leftLabels <- makeLabels(leftLabels, angle = 90, size = 5)
+    topLabels <- makeLabels(topLabels, angle = 0, size = 5)
+    rightLabels <- makeLabels(rightLabels, angle = 270, size = 5)
+    bottomLabels <- makeLabels(bottomLabels, angle = 0, size = 5)
+
+    return(ggMatrixPlot.default(
+        plotList = plotList,
+        nr = nr, 
+        nc = nc,
+        ... = ...,
+        leftLabels = leftLabels,
+        topLabels = topLabels,
+        rightLabels = rightLabels,
+        bottomLabels = bottomLabels
+    ))
+    
+}
+
+#' @export
+ggMatrixPlot.list <- function(plotList = NULL, nr = NULL, nc = NULL,
+                              layout = NULL,
+                                ...,
+                                leftLabels = NULL,
+                                topLabels = NULL,
+                                rightLabels = NULL,
+                                bottomLabels = NULL,
+                                debug = TRUE) {
+    if (is.null(layout)) { # was layout supplied?
+        stop("Either supply plotList as a matrix or provide a layout argument")
+    } else if (!is.matrix(layout)) { # is layout layout a matrix?
+        stop(sprintf("layout must be a matrix but instead was of mode %s and class %s", mode(layout), class(layout)))
+    } else if (length(layout) != length(plotList)) { # does layout have correct length?
+        stop(sprintf("length of layout (%d) does not match length of plotList (%d). Use NULL entries in plotList to specify empty plots.",
+                     length(layout), length(plotList)))
+    } else if (!all(seq_along(layout) %in% layout)) { # does layout have all required values?
+        stop("Layout must consist of unique integers starting from 1.")
+    } else { # layout is good layout
+        plotList <- plotList[layout]
+        nr <- nrow(layout)
+        nc <- ncol(layout)
+        dim(plotList) <- dim(layout)
+    }
+    
+    leftLabels <- makeLabels(leftLabels, angle = 90, size = 5)
+    topLabels <- makeLabels(topLabels, angle = 0, size = 5)
+    rightLabels <- makeLabels(rightLabels, angle = 270, size = 5)
+    bottomLabels <- makeLabels(bottomLabels, angle = 0, size = 5)
+
+    return(ggMatrixPlot.default(
+        plotList = plotList,
+        nr = nr,
+        nc = nc,
+        ... = ...,
+        leftLabels = leftLabels,
+        topLabels = topLabels,
+        rightLabels = rightLabels,
+        bottomLabels = bottomLabels
+    ))
+    
+}
+#' @export
+ggMatrixPlot.default <- function(plotList = NULL, nr = NULL, nc = NULL,
+                                 layout = NULL,
+                                 ...,
+                                 leftLabels = NULL,
+                                 topLabels = NULL,
+                                 rightLabels = NULL,
+                                 bottomLabels = NULL,
+                                 debug = TRUE) {
     
     if (is.null(plotList) && debug) {
-
-        labelsLeftRow <- lapply(seq_len(nr), function(x) makeRect())
-        labelsRightRow <- lapply(seq_len(nr), function(x) makeRect("navajowhite4"))
-        labelsTopCol <- lapply(seq_len(nc), function(x) makeRect("blue"))
-        labelsBottomCol <- lapply(seq_len(nc), function(x) makeRect("orange"))
-        plotMatrix <- lapply(seq_len(nc*nr), function(x) makeRect("green", fill = scales::alpha("springgreen", .5)))
-        dim(plotMatrix) <- c(nr, nc)
-
-    } else {
-    
-        # todo
+        
+        leftLabels <- lapply(seq_len(nr), function(x) makeRect())
+        rightLabels <- lapply(seq_len(nr), function(x) makeRect("navajowhite4"))
+        topLabels <- lapply(seq_len(nc), function(x) makeRect("blue"))
+        bottomLabels <- lapply(seq_len(nc), function(x) makeRect("orange"))
+        plotList <- lapply(seq_len(nc*nr), function(x) makeRect("green", fill = scales::alpha("springgreen", .5)))
+        dim(plotList) <- c(nr, nc)
+        
     }
 
+    hasLeftLab <- !is.null(leftLabels)
+    hasTopLab <- !is.null(topLabels)
+    hasRightLab <- !is.null(rightLabels)
+    hasBottomLab <- !is.null(bottomLabels)
+    
     firstColHeight <- .05
     firstRowWidth <- .05
-
-    firstColWidth <- (1 - firstRowWidth*(addRightLabels+1)) / nc
-    firstRowHeight <- (1 - firstRowWidth*(addBottomLabels+1)) / nr
     
+    firstColWidth <- (1 - firstRowWidth*(hasLeftLab + hasRightLab)) / nc
+    firstRowHeight <- (1 - firstRowWidth*(hasTopLab + hasBottomLab)) / nr
+    # browser()
     # empty plot
     totalGraph <- cowplot::ggdraw(xlim = c(0, 1), ylim = c(0, 1))
     # labels left of plots
-    totalGraph <- addVerticalLabels(totalGraph,
-                                    graphs2add = labelsLeftRow,
-                                    x = 0, 
-                                    yIncrement = firstRowHeight, 
-                                    width = firstRowWidth,
-                                    height = firstRowHeight,
-                                    yOffset = addBottomLabels*firstColHeight)
+    if (hasLeftLab) {
+        totalGraph <- addVerticalLabels(totalGraph,
+                                        graphs2add = leftLabels,
+                                        x = 0, 
+                                        yIncrement = firstRowHeight, 
+                                        width = firstRowWidth,
+                                        height = firstRowHeight,
+                                        yOffset = hasBottomLab*firstColHeight)
+    }
     # labels above plots
-    totalGraph <- addHorizontalLabels(totalGraph, 
-                                      graphs2add = labelsTopCol,
-                                      y = 1 - firstColHeight, 
-                                      xIncrement = firstColWidth, 
-                                      width = firstColWidth, 
-                                      height = firstColHeight,
-                                      xOffset = firstRowWidth)
+    if (hasTopLab) {
+        totalGraph <- addHorizontalLabels(totalGraph, 
+                                          graphs2add = topLabels,
+                                          y = 1 - firstColHeight, 
+                                          xIncrement = firstColWidth, 
+                                          width = firstColWidth, 
+                                          height = firstColHeight,
+                                          xOffset = hasLeftLab*firstRowWidth)
+    }
     
-    if (addRightLabels) { # labels right of plots
+    if (hasRightLab) { # labels right of plots
         totalGraph <- addVerticalLabels(totalGraph, 
-                                        graphs2add = labelsRightRow,
+                                        graphs2add = rightLabels,
                                         x = 1 - firstRowWidth, 
                                         yIncrement = firstRowHeight, 
                                         width = firstRowWidth,
                                         height = firstRowHeight,
-                                        yOffset = addBottomLabels*firstColHeight)
+                                        yOffset = hasBottomLab*firstColHeight)
     }
     
-    if (addBottomLabels) { # labels below plots
+    if (hasBottomLab) { # labels below plots
         totalGraph <- addHorizontalLabels(totalGraph,
-                                          graphs2add = labelsBottomCol,
+                                          graphs2add = bottomLabels,
                                           y = 0, 
                                           xIncrement = firstColWidth, 
                                           width = firstColWidth, 
                                           height = firstColHeight,
-                                          xOffset = firstRowWidth)
+                                          xOffset = hasLeftLab*firstRowWidth)
     }
     
     # actually include plots
-    totalGraph <- addCenterPlots(totalGraph, plotMatrix,
+    totalGraph <- addCenterPlots(totalGraph, plotList,
                                  xIncrement = firstColWidth, 
                                  yIncrement = firstRowHeight, 
-                                 xOffset = firstRowWidth,
-                                 yOffset = addBottomLabels*firstColHeight)
+                                 xOffset = hasLeftLab*firstRowWidth,
+                                 yOffset = hasBottomLab*firstColHeight)
     return (totalGraph)
     
 }
-
